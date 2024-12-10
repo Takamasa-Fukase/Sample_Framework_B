dc92f5e146f3da96fe3cb36d26440f5c1748e064
Takamasa-Fukase <ultrafukase@gmail.com>
2024-12-11 00:16:52 +0900

    Initial Commit

---
+++ Sample_Framework_B/Sample_Framework_B.h
@@ -0,0 +1,18 @@
+//
+//  Sample_Framework_B.h
+//  Sample_Framework_B
+//
+//  Created by ウルトラ深瀬 on 11/12/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sample_Framework_B.
+FOUNDATION_EXPORT double Sample_Framework_BVersionNumber;
+
+//! Project version string for Sample_Framework_B.
+FOUNDATION_EXPORT const unsigned char Sample_Framework_BVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sample_Framework_B/PublicHeader.h>
+
+
